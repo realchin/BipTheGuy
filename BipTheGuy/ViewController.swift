c8be9d8e9361fb723f0ae15cca24ff1ade9c4b64
Timothy Chin <chinti@bc.edu>
2022-02-13 12:32:30 -0500

    Section 4.5

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -31,31 +31,66 @@ class ViewController: UIViewController {
             print("🤬 ERROR: Could not read data from file sound0")
         }
     }
+    
+    func showAlert(title: String, message: String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+        
+    }
 
-    @IBAction func punchButtonPressed(_ sender: UIButton) {
-        let originalFrame = sender.frame // original button frame
-        let widthShrink: CGFloat = 30
-        let heightShrink: CGFloat = 10
-        let smallerFrame = CGRect(x: sender.frame.origin.x + widthShrink, y: sender.frame.origin.y + heightShrink, width: sender.frame.width - (2 * widthShrink), height: sender.frame.height - (2 * heightShrink))
+//    @IBAction func punchButtonPressed(_ sender: UIButton) {
+//        let originalFrame = sender.frame // original button frame
+//        let widthShrink: CGFloat = 30
+//        let heightShrink: CGFloat = 10
+//        let smallerFrame = CGRect(x: sender.frame.origin.x + widthShrink, y: sender.frame.origin.y + heightShrink, width: sender.frame.width - (2 * widthShrink), height: sender.frame.height - (2 * heightShrink))
+//
+//        sender.frame = smallerFrame // shrink before we animate
+//
+//        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0) {
+//            sender.frame = originalFrame
+//        }
+//
+//        let originalImageFrame = imageView.frame
+//        let imageWidthShrink: CGFloat = 20
+//        let imageHeightShrink: CGFloat = 20
+//        let smallerImageFrame = CGRect(x: imageView.frame.origin.x + imageWidthShrink, y: imageView.frame.origin.y + imageHeightShrink, width: imageView.frame.width - (imageWidthShrink * 2), height: imageView.frame.height - (imageHeightShrink * 2))
+//
+//        imageView.frame = smallerImageFrame
+//
+//        playSound(name: "punchSound")
+//
+//        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0) {
+//            self.imageView.frame = originalImageFrame
+//        }
+//    }
+    
+    
+    
+    @IBAction func photoOrCameraPressed(_ sender: UIButton) {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
         
-        sender.frame = smallerFrame // shrink before we animate
-        
-        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0) {
-            sender.frame = originalFrame
+        let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { (_) in
+            print("You clicked Photo Library")
+            // TO-DO: add code to open the Photo Library
         }
         
-        let originalImageFrame = imageView.frame
-        let imageWidthShrink: CGFloat = 20
-        let imageHeightShrink: CGFloat = 20
-        let smallerImageFrame = CGRect(x: imageView.frame.origin.x + imageWidthShrink, y: imageView.frame.origin.y + imageHeightShrink, width: imageView.frame.width - (imageWidthShrink * 2), height: imageView.frame.height - (imageHeightShrink * 2))
-        
-        imageView.frame = smallerImageFrame
-        
-        playSound(name: "punchSound")
-        
-        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10.0) {
-            self.imageView.frame = originalImageFrame
+        let cameraAction = UIAlertAction(title: "Camera", style: .default) { (_) in
+            print("You clicked Camera")
+            // TO-DO: add code to open the Camera
         }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(photoLibraryAction)
+        alertController.addAction(cameraAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+
     }
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
